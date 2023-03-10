cc930727cdb649c9ef038b43e2491e701069844a
four-dollars <54352242+four-dollars@users.noreply.github.com>
2023-03-10 16:16:28 +0800

    two operators computation

--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'values.dart';
 
@@ -20,12 +21,12 @@ List<String> keys = [
   '=',
   'CLEAR',
 ];
-Map<int ,int> weights = {
-  0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,
-  10:4, //numbers
-  11:1,12:1, //+ -
-  13:2,14:2, //* /
-  15:3,16:3, //= clear
+Map<String ,int> weights = {
+  '0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0,
+  '00':4, //numbers
+  '+':1,'-':1, //+ -
+  '*':2,'/':2, //* /
+  '=':3,'CLEAR':3, //= clear
 };
 
 class KeyCard extends StatefulWidget {
@@ -53,7 +54,7 @@ class _KeyCardState extends State<KeyCard> {
               Values.output = 'OUTPUT';
             }
             else if(widget.word == 15 && Values.input != 'INPUT'){ //compute
-              Values.output = Values.input;
+              computation(Values.input);
             }
             else if(Values.input=='INPUT' && widget.word!=15){ //first
               Values.input=keys[widget.word];
@@ -69,6 +70,52 @@ class _KeyCardState extends State<KeyCard> {
         child: Text(keys[widget.word],style: const TextStyle(fontSize: 20),),
       ),
     );
+  }
+
+  computation(String input){
+    double op1=0;
+    double op2=0;
+    int computeType=0;
+    input.runes.forEach((ch) {
+      if(weights[String.fromCharCode(ch)]==0){
+        if(computeType!=0){
+          op2=op2*10+double.parse(String.fromCharCode(ch));
+        }
+        else{
+          op1=op1*10+double.parse(String.fromCharCode(ch));
+        }
+      }//numbers
+      else if(weights[String.fromCharCode(ch)]==1 || weights[String.fromCharCode(ch)]==2){
+        if(String.fromCharCode(ch)=='+'){
+          computeType=1;
+        }
+        else if(String.fromCharCode(ch)=='-'){
+          computeType=2;
+        }
+        else if(String.fromCharCode(ch)=='*'){
+          computeType=3;
+        }
+        else if(String.fromCharCode(ch)=='/'){
+          computeType=4;
+        }
+      }//add and sub
+    });
+    double out=0.0;
+    if (computeType==1){
+      out=op1+op2;
+    }
+    else if (computeType==2){
+      out=op1-op2;
+    }
+    else if (computeType==3){
+      out=op1*op2;
+    }
+    if (computeType==4){
+      out=op1/op2;
+    }
+    setState(() {
+      Values.output=out.toString();
+    });
   }
 }
 
